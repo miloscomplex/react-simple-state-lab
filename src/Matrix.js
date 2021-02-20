28883343dd54f9f2274622bb14208c2af623658f
Michael Sutton <hicallmesutton@gmail.com>
2021-02-19 23:55:39 -0600

    commit tree 715e8bdf0a3d6e0c44188e1190cf0ca2954c3a6e

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
+import Cell from './Cell.js';
 export default class Matrix extends Component {
-  
+
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    //console.log(vals);
+    return vals.map(val => <Cell value={val} />)
   }
-  
+
   genMatrix = () => {
+    console.log(this.props.values);
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +20,9 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
+}
+
+Matrix.defaultProps = {
+  values: [['#F00'],['#F00'],['#F00'],['#F00'],['#F00'],['#F00'],['#F00'],['#F00'],['#F00'],['#F00']]
 }
